9fd33fda678b20c72f93057428580e780f339cdb
juliafortunat24 <juliafortunat24@gmail.com>
2024-11-26 16:18:12 -0300

    commit julia agendamentos

--- paginas/confirmar_agendamento.php
+++ paginas/confirmar_agendamento.php
@@ -59,13 +59,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confirmar Agendamento</title>
-    <link rel="stylesheet" href="../css/tudodeagendamento.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Atma:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/confirmar_agendamento.css">
 </head>
 <body>
-    <h2>Confirme seu Agendamento</h2>
+    <center><h2>Confirme seu Agendamento</h2></center>
     <p><strong>Dia:</strong> <?= ucfirst($dia); ?></p>
     <p><strong>Horário:</strong> <?= $hora; ?></p>
 
